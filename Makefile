50b5d69f20fb359504626b88beb8e3e6b1318222
Will C. <williamjc939@gmail.com>
2026-01-29 17:27:51 -0500

    slop initial

---
+++ Makefile
@@ -0,0 +1,11 @@
+# basic makefile
+
+CC = gcc
+CFLAGS = -Wall -Wextra -g
+ADDRSAN = -fsanitize=address
+LEAKSAN = -fsanitize=leak
+
+all: program
+program: main.o
+	$(CC) $(CFLAGS) $(ADDRSAN) -o program main.o
+main.o: main.c art.h
---
+++ art.h
@@ -0,0 +1,10 @@
+#pragma once
+
+static const char *anim_fish_f1 = " <X>< ";
+static const char *anim_fish_f2 = " <X>  ";
+static const char *anim_fish_f3 = " <X>- ";
+
+static const char **anim_fish_anim[] = { &anim_fish_f1, &anim_fish_f2, &anim_fish_f3 };
+
+static const char *basic_fish_f1 = " <)>< ";
+static const char **basic_fish_anim[] = { &basic_fish_f1 };
